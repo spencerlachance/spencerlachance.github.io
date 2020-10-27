43c9ff93a97211040d4a5c9e3a12ff75f12d14cb
Spencer LaChance <spencerlachance25@gmail.com>
2020-10-26 21:15:36 -0400

    Visualizer page overhaul

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
 $(document).ready( function() {
     $("#header-wrapper").load("header.html");
+
+    $(".fa-caret-square-up").on("click", function() {
+        $(".fa-caret-square-up").toggleClass("rotate");
+        $(".project-gifs").toggleClass("slide");
+    });
 });
